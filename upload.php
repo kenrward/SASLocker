65871b26e43b118c70afe5343eb7e20d50be42b1
kenrward <kenrward@gmail.com>
2016-12-08 12:58:17 -0500

    create containter

--- upload.php
+++ upload.php
@@ -89,7 +89,8 @@ $se=Date($datemask , strtotime("+300 seconds"));
 $exp = $se;
 $act = getenv("STORAGE_ACCOUNT");
 $cont = 'secure';
-$blob = $blob_name;
+$blob = "myblob";
+//$blob = $blob_name;
 $key = getenv("STORAGE_KEY");
  
 $_signature = getSASForBlob($act,$cont,$blob,'b','r',$exp,$key);
