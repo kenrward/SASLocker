a4f83daaa7fed08ca4b752294e91c37e05e855a6
kenrward <kenrward@gmail.com>
2016-12-08 12:06:05 -0500

    create containter

---
+++ containercreate.php
@@ -0,0 +1,58 @@
+<html>
+ <head>
+  <title>SAS Locker Proof of Concept</title>
+ </head>
+ <?php
+ require_once 'vendor/autoload.php';
+use WindowsAzure\Common\ServicesBuilder;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use MicrosoftAzure\Storage\Common\ServiceException;
+
+$connectionString = getenv("STORAGE_CONNECTION_STR");
+
+// Create blob REST proxy.
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
+$createContainerOptions = new CreateContainerOptions();
+
+// Set public access policy. Possible values are
+// PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+// CONTAINER_AND_BLOBS:
+// Specifies full public read access for container and blob data.
+// proxys can enumerate blobs within the container via anonymous
+// request, but cannot enumerate containers within the storage account.
+//
+// BLOBS_ONLY:
+// Specifies public read access for blobs. Blob data within this
+// container can be read via anonymous request, but container data is not
+// available. proxys cannot enumerate blobs within the container via
+// anonymous request.
+// If this value is not specified in the request, container data is
+// private to the account owner.
+$createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+// Set container metadata.
+$createContainerOptions->addMetaData("key1", "value1");
+$createContainerOptions->addMetaData("key2", "value2");
+	
+try    {
+    // Create container.
+    $blobRestProxy->createContainer("securelocker");
+}
+catch(ServiceException $e){
+    // Handle exception based on error codes and messages.
+    // Error codes and messages are here:
+    // http://msdn.microsoft.com/library/azure/dd179439.aspx
+    $code = $e->getCode();
+    $error_message = $e->getMessage();
+    echo $code.": ".$error_message."<br />";
+	
+	
+}
+
+ ?>
+ <body>
+ <?php echo '<p>Conainter Created</p>'; ?> 
+
+ </body>
+</html>
